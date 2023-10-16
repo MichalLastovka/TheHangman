08b5de98977a24423ca49e73e7f34bf7880ebb0f
lasmich@protonmail.com <lasmich@protonmail.com>
2023-10-16 13:30:21 +0200

    New version with multiple screens

--- app/src/main/java/eu/example/thehangman/Menu.kt
+++ app/src/main/java/eu/example/thehangman/Menu.kt
@@ -2,30 +2,34 @@ package eu.example.thehangman
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatImageButton
 import java.util.Locale
 
 class Menu : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        var langSettings = "CZE"
         val newGameButton = findViewById<Button>(R.id.newGame)
         newGameButton.setOnClickListener{
             val intent = Intent(this, GameSettings::class.java)
             startActivity(intent)
         }
+
         val languageButton = findViewById<AppCompatImageButton>(R.id.language)
+        println(resources.configuration.locales.get(0))
+
         languageButton.setOnClickListener{
+            println(Locale.getDefault().displayLanguage)
             var locale : Locale
-            if (langSettings == "CZE"){
-                languageButton.setImageResource(R.drawable.eng)
+            if (resources.configuration.locales.get(0) == Locale("en")){
                 locale = Locale("cs")
-                langSettings = "ENG"
                 var res = resources
                 var dm = res.displayMetrics
                 var conf = res.configuration
@@ -33,10 +37,10 @@ class Menu : AppCompatActivity() {
                 res.updateConfiguration(conf, dm)
                 var refresh = Intent(Menu@this, Menu::class.java)
                 startActivity(refresh)
+                languageButton.setImageResource(R.drawable.eng)
             }else{
                 languageButton.setImageResource(R.drawable.cze)
                 locale = Locale("en")
-                langSettings = "CZE"
                 var res = resources
                 var dm = res.displayMetrics
                 var conf = res.configuration
@@ -44,8 +48,8 @@ class Menu : AppCompatActivity() {
                 res.updateConfiguration(conf, dm)
                 var refresh = Intent(Menu@this, Menu::class.java)
                 startActivity(refresh)
+                languageButton.setImageResource(R.drawable.cze)
             }
-
         }
     }
 }
